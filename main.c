6df7b6c3ac26c5914cb85112933bc8d8e348124a
Akshit Sharma <akshit9sharma@gmail.com>
2017-02-23 21:06:33 +0530

    malloc added  and other changes

--- main.c
+++ main.c
@@ -4,9 +4,10 @@
 #include<stdio.h>
 #include<time.h>
 
-#define numThreads 1000000
-#define arraySize 1000000
-#define blockSize 1000000
+
+#define numThreads 100000000
+#define arraySize 100000000
+#define blockSize 100000000
 
 
 void printArray(int* array, long int size)
@@ -25,18 +26,26 @@ __global__ void incrementNaive(int *g)
 int main()
 {
 	clock_t start, end;
-	printf("%d total threads in %d blocks writing into %d array elements\n", numThreads, numThreads/blockSize, arraySize);
-	int h_array[arraySize];
-	const int arrayBytes = arraySize * sizeof(int);
+	printf("%d total threads in %d blocks writing into %d array elements\n", numThreads, numThreads / blockSize, arraySize);
+	int * h_array;
+	h_array = (int *) malloc(sizeof(int) * arraySize);
+	const size_t arrayBytes = arraySize * sizeof(int);
 	int *d_array;
+	printf("Max int is %d and size of one is %d \n", INT_MAX, sizeof(int));
+	printf("Using size : %ld B or %ld KB or %ld MB or %ld GB \n", arrayBytes,
+																arrayBytes >> 10,
+																arrayBytes >> 20,
+																arrayBytes >> 30
+																);
 	cudaMalloc((void**)&d_array, arrayBytes);
 	cudaMemset((void*)d_array, 0, arrayBytes);
 	start = clock();
-	incrementNaive <<<numThreads / blockSize, blockSize >>> (d_array);
+	incrementNaive <<<blockSize / numThreads, numThreads>>> (d_array);
+	cudaDeviceSynchronize();
 	end = clock();
 	cudaMemcpy(h_array, d_array, arrayBytes, cudaMemcpyDeviceToHost);
-	printArray(h_array,arraySize);
-	printf("\nTime elapsed=%d\n", (double)end-start);
+	//printArray(h_array, arraySize);
+	printf("\nTime elapsed=%lf\n", (double)end - start);
 	cudaFree(d_array);
 	return 0;
 }
